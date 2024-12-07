72b728c35bc63c9cea44bdd4a8cb85a97aae61af
Alexei Pancratov <drivergear@gmail.com>
2024-12-07 14:19:19 -0500

    Initial commit

---
+++ BeerSender.Domain/Boxes/Box.cs
@@ -0,0 +1,30 @@
+﻿namespace BeerSender.Domain.Boxes;
+
+public class Box
+{
+}
+
+// Commands
+public record CreateBox(Guid BoxId, int DesiredNumberOfSpots);
+
+public record AddShippingLabel(Guid BoxId, string TrackingCode, Carrier Carrier);
+
+// Events
+public record BoxCreated(int ActualNumberOfSpots);
+
+public record ShippingLabelAdded(string TrackingCode, Carrier Carrier);
+
+public record ShippingLabelFailedToAdd(ShippingLabelFailedToAdd.FailReason Reason)
+{
+    public enum FailReason
+    {
+        TrackingCodeInvalid
+    }
+}
+
+public enum Carrier
+{
+    Ups,
+    FedEx,
+    CanadaPost
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# BeerSender - Event Sourcing Demo
+
+This projects serves as a demo of the Event Sourcing pattern.
