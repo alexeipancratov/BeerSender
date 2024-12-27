45b687a1164ff33a7fef3b9ac1d75ef2f1370117
Alexei Pancratov <drivergear@gmail.com>
2024-12-27 17:45:45 -0500

    Added EventStore and Web projects

---
+++ BeerSender.EventStore/EventStore.cs
@@ -0,0 +1,13 @@
+﻿using BeerSender.Domain;
+
+namespace BeerSender.EventStore;
+
+public class EventStore(EventStoreConnectionFactory connectionFactory)
+{
+    private readonly EventStoreConnectionFactory _connectionFactory = connectionFactory;
+
+    public IReadOnlyList<StoredEvent> GetEvents(Guid aggregateId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/BeerSender.Web/Controllers/BoxController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerSender.Web.Controllers;
+
+[ApiController]
+[Route("api/command/[controller]")]
+public class BoxController : ControllerBase
+{
+    
+}
